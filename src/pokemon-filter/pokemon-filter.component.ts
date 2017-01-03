1e77fddd21016c22e4cd64aa8110439dcd0a1d56
Clapinton <eric.olivetree@gmail.com>
2017-01-02 19:11:25 -0800

    Skeleton for filter component

--- src/pokemon-filter/pokemon-filter.component.ts
+++ src/pokemon-filter/pokemon-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { PokemonListItem } from '../pokemon';
 import { searchByName, searchByNumber } from '../helpers';
 
@@ -13,19 +13,38 @@ export class PokemonFilterComponent {
   @Input() allPokemon: PokemonListItem[];
   @Output() filterPokemon = new EventEmitter();
 
-  searchPokemon(searchTerm) {
+  filtered: PokemonListItem[];
 
-      let filtered = []
-
-    if (searchTerm === '') {
-      filtered = this.allPokemon;
-    } else if (Number(searchTerm)*0 === 0) {
-      filtered = searchByNumber(this.allPokemon, Number(searchTerm));
-    } else {
-      filtered = searchByName(this.allPokemon, searchTerm);
-    }
-
-    this.filterPokemon.emit(filtered);
+  ngOnChanges(changes) {
+    this.filtered = this.allPokemon;
+    console.log("filter component just updated with ", changes);
+    
   }
+
+  executeFilters(searchTerm) {
+    this.filtered = this.allPokemon;
+
+    // Filter by filters first since it's faster time complexity than performing string search.
+    this.filterByFilters();
+    this.filterBySearch(searchTerm);
+
+    this.filterPokemon.emit(this.filtered);
+  }
+  
+  filterByFilters() {
+    this.filtered = this.allPokemon;
+  }
+
+  filterBySearch(searchTerm) {
+    // Do not alter this.filtered array if user deleted the search query
+    if (searchTerm.length === 0) return;
+
+    if (Number(searchTerm)*0 === 0) {
+      this.filtered = searchByNumber(this.filtered, Number(searchTerm));
+    } else {
+      this.filtered = searchByName(this.filtered, searchTerm);
+    }
+  }
+
 
 }
