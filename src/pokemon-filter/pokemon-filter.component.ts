f83c9a6403d217f94f09841090ef56a3413d5b90
Clapinton <eric.olivetree@gmail.com>
2017-01-03 22:17:51 -0800

    Basic filtering and search working. Able to select one at a time.

--- src/pokemon-filter/pokemon-filter.component.ts
+++ src/pokemon-filter/pokemon-filter.component.ts
@@ -14,6 +14,8 @@ export class PokemonFilterComponent {
   @Input() allPokemon: PokemonListItem[];
   @Output() filterPokemon = new EventEmitter();
 
+  pkmnQuery: String = "";
+  pkmnTypeFilter: number[] = [];
   filtered: PokemonListItem[];
 
   // Needed to make allTypes available to the HTML template
@@ -21,41 +23,56 @@ export class PokemonFilterComponent {
 
   ngOnChanges(changes) {
     this.filtered = this.allPokemon;
-    console.log("filter component just updated with ", changes);
-    console.log(allTypes);
-    
-    
   }
 
-  executeFilters(searchTerm) {
-    this.filtered = this.allPokemon;
-
-    // Filter by type first since it's faster time complexity than performing string search.
-    this.filterByType();
-    this.filterBySearch(searchTerm);
-
-    this.filterPokemon.emit(this.filtered);
-  }
-  
-  filterByType() {
-    this.filtered = this.allPokemon;
+  onPkmnQueryChange(searchTerm) {
+    this.pkmnQuery = searchTerm;
+    this.executeFilters();
   }
 
   onFilterByType(selectedType) {
-    console.log(mappedPkmnTypes[selectedType]);
-    
+    if (selectedType === "all") {
+      this.pkmnTypeFilter = [];
+    } else {
+      this.pkmnTypeFilter = mappedPkmnTypes[selectedType];
+    }
+    this.executeFilters();
   }
 
-  filterBySearch(searchTerm) {
-    // Do not alter this.filtered array if user deleted the search query
-    if (searchTerm.length === 0) return;
+  executeFilters() {
 
-    if (Number(searchTerm)*0 === 0) {
-      this.filtered = searchByNumber(this.filtered, Number(searchTerm));
+    // Filter by type first since it's faster time complexity than performing string search.
+    this.filterByType();
+    this.filterBySearch();
+
+    this.filterPokemon.emit(this.filtered);
+  }
+
+  filterByType() {
+    if (this.pkmnTypeFilter.length === 0) {
+      //If no type filter selected, then proceed to filter by query using allPokemon
+      this.filtered = this.allPokemon; 
     } else {
-      this.filtered = searchByName(this.filtered, searchTerm);
+      //If a type filter is selected, add the pkmn to the array 
+      this.filtered = [];
+      let idx;
+      this.pkmnTypeFilter.forEach( pkmnId => {
+        //The pkmnId is translated to the array index by subtracting 1 (ex.: Bulba becomes 0)
+        idx = pkmnId - 1;
+        this.filtered.push(this.allPokemon[idx]);
+      });
     }
   }
+  
+  filterBySearch() {
+    // Do not alter this.filtered array if user deleted the search query
+    if (this.pkmnQuery.length === 0) return;
 
+    if (Number(this.pkmnQuery)*0 === 0) {
+      this.filtered = searchByNumber(this.filtered, Number(this.pkmnQuery));
+    } else {
+      this.filtered = searchByName(this.filtered, this.pkmnQuery);
+    }
+  }
 
 }
